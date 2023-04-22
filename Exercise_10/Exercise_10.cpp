c0522a549b139fcb6f2e47051e6f4971e7c98b31
tihung99123 <crossfire294@gmail.com>
2023-04-22 21:46:59 +0700

    add E_9, E_10

---
+++ Exercise_10/Exercise_10.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+//Viết chương trình tìm USCLN và BSCNN. (Viết chương trình có sử dụng hàm).
+int USCLN(int a, int b) { //khai báo hàm tìm ước số chung lơn nhất
+	if (b == 0) { //nếu b bằng 0 thì
+		return a; //xuất a ra
+	}
+	else { //không phải thì
+		return USCLN(b, a % b); //xuất ước số chung theo công thức: a chia b dư số lẻ hoặc chẵn
+	}
+}
+
+int BSCNN(int a, int b) { // khai boá hàm tìm bội số chung nhỏ nhất
+	return (a * b) / USCLN(a, b); //xuất bội số chung nhỏ nhất theo công thức: (a * b) / ước số chung của a và b
+}
+
+int main()
+{
+	int num1, num2; //khai báo biến số1, số2
+	cout << "Nhap 2 so Bat Ki: ";
+	cin >> num1 >> num2;
+	
+	int USCLN_result = USCLN(num1, num2);
+	int BSCNN_result = BSCNN(num1, num2);
+
+	cout << "USCLN: " << USCLN_result << endl;
+	cout << "BSCNN: " << BSCNN_result << endl;
+}
---
+++ Exercise_9/Exercise_9.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+//Viết chương trình tìm max của 2 số a, b. (Viết chương trình đơn và viết có sử dụng hàm).
+
+int func_Max(int a, int b) { //khởi tạo (hàm) max tìm số lớn nhất
+    int max; //khai báo biến thứ 3 tên là max
+    if (a > b) { //nếu a lớn hơn b thì a sẽ là số lớn nhất
+        max = a;
+    }
+    else { //nếu không phải thì b sẽ là số lớn nhất
+        max = b;
+    }
+    return max; //trả kết quả vào hàm
+}
+
+int main()
+{
+    int a, b; //khai báo 2 biến a, b để so sánh
+
+    int max; //khai báo biến max để chạy chương trình đơn
+    int func_max; //khai báo biến max để chạy chương trình sử dụng hàm
+
+    cout << "Nhap a "; cin >> a;
+    cout << "Nhap b "; cin >> b;
+
+
+    cout << "Chuong Trinh Don\n";
+    if (a > b) { //nếu a lớn hơn b thì a sẽ là số lớn nhất
+        max = a;
+    }
+    else { //nếu không phải thì b sẽ là số lớn nhất
+        max = b;
+    }
+    cout << "So Lon Nhat: " << max << endl;
+
+    //========================================================
+    cout << "Chuong Trinh Su Dung Ham\n";
+    func_max = func_Max(a, b);
+    cout << "So Lon Nhat: " << func_max << endl;
+}
