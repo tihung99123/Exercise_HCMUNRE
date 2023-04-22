d4e842898bdb61ca902e0dccaaae2cbcf2f5996a
tihung99123 <crossfire294@gmail.com>
2023-04-22 20:45:42 +0700

    rename E_4 add E_6

---
+++ Exercise_6/Exercise_6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+//Viết chương trình giải phương trình bậc nhất ax + b = 0
+int main()
+{
+	double a, b, x;//tạo biến a, b theo giá trị số thập phân
+	cout << "Nhap a: ";
+	cin >> a;
+	cout << "Nhap b: ";
+	cin >> b;
+
+	x = -b / a; //Tìm x thôi!!
+	cout << "Nghiem X= " << x << endl;
+
+}
