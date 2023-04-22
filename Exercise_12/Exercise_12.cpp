78a33f8b7e7162794368d0217cb3a2e7f3408e41
tihung99123 <crossfire294@gmail.com>
2023-04-22 22:12:08 +0700

    add E_12

---
+++ Exercise_12/Exercise_12.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+//Tạo máy tính tay.
+
+int main()
+{
+    double num1, num2; //khai báo biến sô1, sô2
+    char op;
+    cout << "Nhap 2 so vao phep tinh "; cin >> num1 >> num2;
+    cout << "Nhap phep tinh (+,-,*,/): "; cin >> op;
+
+    double result;
+    switch (op) //dùng cấu trúc switch
+    {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+
+        default:
+            break;
+    }
+    cout << "Ket Qua: " << result << endl;
+}
